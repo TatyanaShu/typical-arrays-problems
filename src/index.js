25386e15a2be109647463cc17712af52ea46bce7
TatyanaShu <tashu529@gmail.com>
2021-03-13 11:24:13 +0300

    feat: update 3 fails json &js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  min=arr[0];
+  for (let i=0; i< array.length; i++) {
+    if(min>arr[i]){
+      min=arr[i]
+    }
+  }
+
 }
 
 exports.max = function max (array) {
-  return 0;
+  max=0;
+  for (let i=0; i< array.length; i++) {
+    if(max<arr[i]){
+      max=arr[i]
+    }
+  }
+
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let x;
+  for (let i=0; i< array.length; i++) {
+    x+=arr[i];
+  }
+
 }
